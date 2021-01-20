ba9862f10ea70dd0c7e8faec8f8feba6dd7aaa2a
Swando97 <adam_swanny@hotmail.com>
2021-01-19 19:36:09 -0800

    changed App.js to have headins and picture

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import picture from './img/nardwuar.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Adam Swanson
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </h1>
+        <h2>Frontend Web Developer</h2>
+        <img src={picture} alt="profilePic" />
       </header>
     </div>
   );
