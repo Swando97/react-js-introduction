4a4dcfe2df299b4ce7f5c39fa800fbde7db45e41
Swando97 <adam_swanny@hotmail.com>
2021-01-20 16:03:45 -0800

    Image layout define

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import picture from './img/nardwuar.png';
 import AdamPicture from './img/adamswanson.png';
 import LoopiPicture from './img/loopilasso.png';
+import CoffeePicture from './img/coffee.png';
 import './App.css';
 
 function App() {
@@ -13,16 +14,20 @@ function App() {
         <p> A gallery of Adam Swanson's recent Photoshop edits.</p>
         <div class="row">
           <div class="column">
-            <img src={picture} alt="NardwuarPic" />
-            <img src={AdamPicture} alt="AdamPic" />
-            <img src={LoopiPicture} alt="LoopilassoPic" />
+            <img src={CoffeePicture} alt="AdamPic" />
           </div>
           <div class="column">
-            <img src={picture} alt="NardwuarPic" />
-            <img src={AdamPicture} alt="AdamPic" />
-            <img src={LoopiPicture} alt="LoopilassoPic" />
+            <img src={CoffeePicture} alt="LoopiPic" />
+          </div>
+          <div class="column">
+            <img src={CoffeePicture} alt="CoffeePic" />
+          </div>
+          <div class="column">
+            <img src={CoffeePicture} alt="NardwuarPic" />
           </div>
         </div>
+            
+            
       </header>
     </div>
   );
